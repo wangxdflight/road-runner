65567e2439b66b0b92f0c00a12560cf706507024
wangxdflight <wxiaodong@gmail.com>
2020-10-31 23:59:39 -0700

    .

--- core/src/main/kotlin/com/acmerobotics/roadrunner/followers/TrajectoryFollower.kt
+++ core/src/main/kotlin/com/acmerobotics/roadrunner/followers/TrajectoryFollower.kt
@@ -4,7 +4,6 @@ import com.acmerobotics.roadrunner.drive.DriveSignal
 import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.acmerobotics.roadrunner.trajectory.Trajectory
 import com.acmerobotics.roadrunner.trajectory.TrajectoryMarker
-import com.acmerobotics.roadrunner.util.Log
 import com.acmerobotics.roadrunner.util.Angle
 import com.acmerobotics.roadrunner.util.Log
 
